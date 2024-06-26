19871862368683fde97c77ea9b1dcbfe6dd6b6c4
Hwangsedong <saedongh5@naver.com>
2024-06-27 00:00:05 +0900

    feat: ErrorCode 추가(#15)

--- src/main/java/com/travelgo/backend/global/exception/constant/ErrorCode.java
+++ src/main/java/com/travelgo/backend/global/exception/constant/ErrorCode.java
@@ -18,6 +18,8 @@ public enum ErrorCode {
     NOT_FOUND_ATTRACTION(HttpStatus.NOT_FOUND, "업체를 찾을 수 없습니다."),
     NOT_FOUND_REVIEW(HttpStatus.NOT_FOUND, "리뷰를 찾을 수 없습니다."),
     NOT_FOUND_ITEM(HttpStatus.NOT_FOUND, "아이템을 찾을 수 없습니다."),
+    NOT_FOUND_GEO(HttpStatus.NOT_FOUND, "위경도로 지역을 찾을 수 없습니다."),
+    NOT_FOUND_KAKAO(HttpStatus.NOT_FOUND, "카카오 API에서 올바른 응답을 가져오지 못했습니다."),
 
     DUPLICATED_USER(HttpStatus.CREATED, "이미 존재하는 회원입니다."),
     DUPLICATED_ITEM(HttpStatus.CREATED, "이미 존재하는 아이템입니다."),
@@ -25,6 +27,7 @@ public enum ErrorCode {
 
     BAD_REQUEST(HttpStatus.BAD_REQUEST, "정상적인 요청이 아닙니다."),
     UNAUTHORIZED(HttpStatus.UNAUTHORIZED, "인증이 필요합니다."),
+    UNAUTHORIZED_KAKAO(HttpStatus.UNAUTHORIZED, "카카오 API 호출 중 오류 발생"),
     UNKNOWN(HttpStatus.INTERNAL_SERVER_ERROR, "알수없는 에러 발생.");
 
     private final HttpStatus code;
