4e639a2e106bc7065ad1eb864c7e1c6bd2d56cc2
hyeonsuk <vkdnjdjxor@naver.com>
2024-07-30 17:04:10 +0900

    style: 디렉토리 경로 fix

--- src/main/java/com/travelgo/backend/config/OctetStreamReadMsgConverter.java
+++ src/main/java/com/travelgo/backend/config/OctetStreamReadMsgConverter.java
@@ -1,4 +1,4 @@
-package com.travelgo.backend.config.converter;
+package com.travelgo.backend.config;
 
 import com.fasterxml.jackson.databind.ObjectMapper;
 import org.springframework.beans.factory.annotation.Autowired;
